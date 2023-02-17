44d620fc68ebbc66b09017885c7f14d6bf595003
Ruannino <105193525+ruannino@users.noreply.github.com>
2023-02-16 21:42:11 -0300

    Initial commit

---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+// Uma sentença de código termina com ;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Primeiro Programa Parte #01!");
+        System.out.println("Primeiro Programa Parte #02!");
+    }
+}
+
+/*
+*   https://docs.oracle.com/javase/7/docs/api/overview-summary.html
+*/
