c2023ed5184d85402bfe806c04dac34f7d9072f8
Ruannino <105193525+ruannino@users.noreply.github.com>
2023-03-25 23:43:57 -0300

    Commit 25.03.2023

--- src/fundamentos/operadores/Relacionais.java
+++ src/fundamentos/operadores/Relacionais.java
@@ -8,5 +8,17 @@ public class Relacionais {
         int b = 'a';
 
         System.out.println(a == b);
+        System.out.println(30 != 7);
+        System.out.println(3 > 4);
+        System.out.println(3 >= 4);
+        System.out.println(3 < 7);
+        System.out.println(30 <= 7);
+
+        double nota = 7.3;
+        boolean bomComportamento = true;
+        boolean passouPorMedia = nota >= 7;
+        boolean temDesconto = bomComportamento && passouPorMedia;
+
+        System.out.println("Tem desconto ? " + temDesconto);
     }
 }
