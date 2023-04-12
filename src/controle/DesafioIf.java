2568526fb847bf273749508a92019066829034d0
Ruannino <105193525+ruannino@users.noreply.github.com>
2023-04-11 23:43:47 -0300

    Commit 11.04.2023

---
+++ src/controle/DesafioIf.java
@@ -0,0 +1,17 @@
+// Estruturas de controle não se encerram com ";"
+
+package controle;
+
+public class DesafioIf {
+
+    public static void main(String[] args) {
+
+        double nota = 1.3;
+
+        if(nota >= 9.0) {
+            System.out.println("Quadro de Honta!");
+            System.out.println("Você é fera!!!");
+        }
+
+    }
+}
