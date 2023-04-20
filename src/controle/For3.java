7555e60679ee9f652399d3cc656cb6d954b82ba7
Ruannino <105193525+ruannino@users.noreply.github.com>
2023-04-19 22:41:23 -0300

    Commit 19.04.2023

---
+++ src/controle/For3.java
@@ -0,0 +1,22 @@
+package controle;
+
+public class For3 {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.printf("[%d %d]\n", i, j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+//
+// for(int i = 0; i < 10; i++) {
+//        System.out.println(i);
+//        }
+//        int i = 0;
+//        System.out.println("Saiu do for...");
+//        System.out.println(i);
